0ee4569c87909da4a9d45f351943c55a42ad191f
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2024-11-07 09:12:47 +0100

    terminé: ajout fonction victory

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 print('premier code')
+list = ["","","","","","","","",""]
+def victory(list):
+    # checkerais l'ensemble des combinaisons possible pour gagner
+    return True
 
 is_ended = False
 
 while is_ended == False: 
     new_value = int(input('veuillez entrer une coordonnée: '))
+
